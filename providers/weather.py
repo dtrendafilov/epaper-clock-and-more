a8b3ef6a54b77561b146cd7953c947d5ec20ee01
Dimitar Trendafilov <trendafilov.dn@gmail.com>
2020-01-05 14:08:53 +0200

    Enhanced weather report
    - added apparent temperature, wind speed, wind
      gust and Beaufort scale for the wind

--- providers/weather.py
+++ providers/weather.py
@@ -5,17 +5,40 @@ from .acquire import Acquire
 
 import logging
 import requests
+import bisect
 from collections import namedtuple
 
 
-WeatherTuple = namedtuple('Weather', ['temp', 'temp_min', 'temp_max', 'icon', 'summary', 'forecast_summary', 'nearest_storm_distance', 'alert_title', 'alert_description'])
+WeatherTuple = namedtuple('Weather', ['temp', 'temp_min', 'temp_max', 'icon',
+    'summary', 'forecast_summary', 'nearest_storm_distance', 'alert_title',
+    'alert_description', 'wind_speed', 'wind_gust', 'apparent_temp', 'beaufort'])
+
+
+BEAUFORT_SCALE = (
+        (1, 2),
+        (3, 3),
+        (5, 6),
+        (7, 9),
+        (10, 11),
+        (12, 14),
+        (15, 17),
+        (19, 21),
+        (23, 24),
+        (27, 28),
+        (31, 32),
+        )
+
+def beaufort_scale(speed, gust):
+    return bisect.bisect_left(BEAUFORT_SCALE, (speed, gust))
+    
 
 
 class Weather(Acquire):
 
 
     DEFAULT = WeatherTuple(temp=-99, temp_min=-99, temp_max=-99, icon='n/a', summary='n/a',
-                           forecast_summary='n/a', nearest_storm_distance=None, alert_title=None, alert_description=None)
+                           forecast_summary='n/a', nearest_storm_distance=None, alert_title=None, alert_description=None,
+                           wind_speed=-1, wind_gust=-1, apparent_temp=-1, beaufort=-1)
 
 
     def __init__(self, key, lat, lon, units, cache_ttl):
@@ -46,7 +69,6 @@ class Weather(Acquire):
                 ),
                 params = {
                     "units" : self.units,
-                    "exclude" : "minutely,hourly,flags"
                 }
             )
             return r
@@ -81,6 +103,10 @@ class Weather(Acquire):
                 nearest_storm_distance=c.get('nearestStormDistance', None),
                 alert_title=a[0]['title'] if a is not None else None,
                 alert_description=a[0]['description'] if a is not None else None,
+                wind_speed=c['windSpeed'],
+                wind_gust=c['windGust'],
+                apparent_temp=c['apparentTemperature'],
+                beaufort=beaufort_scale(c['windSpeed'], c['windGust']),
             )
 
         except Exception as e:
